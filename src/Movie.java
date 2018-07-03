e7c5e1523af82e2d44006c00db771ab6ce11d78a
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-07-03 21:58:25 +0200

    Movie & Store

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
-
 public class Movie {
+    private String director;
+    private String title;
+    private String category;
+    private String production;
+    private int year;
+    private int lenght;
+
+    Movie(String director,
+            String title,
+            String category,
+            String production,
+            int year,
+            int lenght) {
+        this.director = director;
+        this.title = title;
+        this.category = category;
+        this.production = production;
+        this.year = year;
+        this.lenght = lenght;
+        this.rented = false;
+    }
+
+    private boolean rented;
+
+    public boolean checkAvailability() {
+        return rented;
+    }
+
+    public void rent() {
+        rented = true;
+    }
+
+    public void giveBack() {
+        rented = false;
+    }
+
+    public void showMovie() {
+        System.out.println("Tytuł: " + title);
+        System.out.println("Reżyser: " + director);
+        System.out.println("Kategoria: " + category);
+        System.out.println("Produkcja: " + production);
+        System.out.println("Rok: " + year);
+        System.out.println("Długość filmu: " + lenght);
+    }
+
+    public boolean checkTitle(String tytul) {
+        return this.title == tytul;
+    }
+
 }
--- src/Store.java
+++ src/Store.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
-
 public class Store {
+
+    private Movie[] moviesList;
+
+    Store(int value) {
+        moviesList = new Movie[value];
+    }
+
+    void addMovie(Movie movie, int index) {
+        moviesList[index] = movie;
+    }
+
+    public void showStore() {
+        for (int i = 0; i < moviesList.length; i++) {
+            moviesList[i].showMovie();
+            System.out.println();
+        }
+    }
+
+    public boolean checkExist(String title) {
+        for (int i = 0; i < moviesList.length; i++) {
+            if (moviesList[i].checkTitle(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int idExist(String title) {
+        for (int i = 0; i < moviesList.length; i++) {
+            if (moviesList[i].checkTitle(title)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void movieRent(String title) {
+        int index = idExist(title);
+        if (index != -1) {
+            if (moviesList[index].checkAvailability()) {
+                System.out.println("Film został już wypożyczony!.");
+            } else {
+                moviesList[index].rent();
+                moviesList[index].showMovie();
+            }
+        } else {
+            System.out.println("Niestety, wybrany film jest niedostępny :(");
+        }
+    }
+
+    public void returnMovie(String title) {
+        int index = idExist(title);
+        if (index != -1) {
+            moviesList[index].giveBack();
+        } else {
+            System.out.println("Niestety, takiego filmu nie ma w naszej wypożyczalni!");
+        }
+    }
+
 }
