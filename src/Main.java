a2225298a7c72bc70814c2b90cfdaed65322d417
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-07-04 19:14:39 +0200

    Sortowanie w CSV, Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,64 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String csvFile = "src/listMovie.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
+
+
+        try {
+            long countOfLines = Files.lines(Paths.get(new File(csvFile).getPath())).count();
+
+            Store store = new Store ((int) countOfLines - 1);
+
+            br = new BufferedReader(new FileReader(csvFile));
+
+            boolean skipFirtLine = true;
+            int currentLine = 0;
+            while ((line = br.readLine()) != null) {
+                if (skipFirtLine) {
+                    skipFirtLine = false;
+                    continue;
+                }
+
+                String[] string = line.split(cvsSplitBy);
+
+                Movie movie = new Movie(
+                        string[0].trim(),
+                        string[1].trim(),
+                        string[2].trim(),
+                        Integer.parseInt(string[3].trim()),
+                        string[4].trim(),
+                        string[5].trim());
+
+                store.addMovie(movie, currentLine);
+                currentLine++;
+
+            }
+
+            store.showStore();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
     }
+
 }
